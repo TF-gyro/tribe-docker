868b3644bf109cecd325dc25707cb461cddf3750
Apurv Jyotirmay <mail@jyotirmay.dev>
2025-02-05 17:23:45 +0530

    updated script for better UX

--- build.sh
+++ build.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
-VER=$(curl https://api.github.com/repos/tribe-framework/flame-junction-dist/tags | jq -r '.[0].name')
+VER=$(curl -s https://api.github.com/repos/tribe-framework/flame-junction-dist/tags | jq -r '.[0].name')
 
-echo "Building with junction $VER"
-sleep 3s;
-sed -i "s|flame-junction-dist.*.tar.gz|flame-junction-dist/archive/refs/tags/$VER.tar.gz|" Dockerfile
+read -p "Building with Junction $VER. Continue? (Y/n) " u_confirm
 
-if [ "$1" == "--no-cache" ]; then
-    # don't use cache
-    docker build --no-cache -t tribeframework/tribe:latest .
-else
-    # use cache
-    docker build -t tribeframework/tribe:latest .
+if [ $u_confirm = "Y" ]; then
+    sed -i "s|flame-junction-dist.*.tar.gz|flame-junction-dist/archive/refs/tags/$VER.tar.gz|" Dockerfile
+
+    if [ "$1" == "--no-cache" ]; then
+        # don't use cache
+        docker build --no-cache -t tribeframework/tribe:latest .
+    else
+        # use cache
+        docker build -t tribeframework/tribe:latest .
+    fi
 fi
+
