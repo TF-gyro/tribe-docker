e8972c06fee847817ff455ce402bc5e12b26e191
Apurv Jyotirmay <mail@jyotirmay.dev>
2025-02-05 17:17:41 +0530

    better feedback for script

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 
 VER=$(curl https://api.github.com/repos/tribe-framework/flame-junction-dist/tags | jq -r '.[0].name')
 
+echo "Building with junction $VER"
+sleep 3s;
 sed -i "s|flame-junction-dist.*.tar.gz|flame-junction-dist/archive/refs/tags/$VER.tar.gz|" Dockerfile
 
 if [ "$1" == "--no-cache" ]; then
